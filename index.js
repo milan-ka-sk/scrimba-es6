deff10390e08f3e786a0aedf95749bb3912cbe79
Milan Koniar <milankoniar@gmail.com>
2019-04-06 16:29:56 +0200

    default params

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-function add(...nums) {
-    
-    let total;
-    
-    // old
-    // total = nums.reduce(function(x,y){
-    //     return x + y;
-    // })
+// function add(numArray) {
+//     //numArray = numArray || [];  // alr1
+//     numArray = typeof numArray !== 'undefined' ? numArray : []; // alt 2
+//     // https://stackoverflow.com/questions/894860/set-a-default-parameter-value-for-a-javascript-function
 
-    // es6
-    // total = nums.reduce((x,y) => {
-    //     return x + y;
-    // })
+//     let total = 0;
+//     numArray.forEach((element) => {
+//         total += element;
+//     });
+    
+//     console.log(total);
+// }
 
-    // if it is just a one line return we can shorten even it so:
-    total = nums.reduce((x, y) => x + y);
+// add();
+
+function add(numArray = []) {
+    let total = 0;
+    numArray.forEach((element) => {
+        total += element;
+    });
     
     console.log(total);
 }
 
-add(4, 5, 7, 8, 12)
+add();
