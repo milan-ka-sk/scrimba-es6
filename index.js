be4b6215a33d5f74d6919cbe123254848441c06b
Milan Koniar <milankoniar@gmail.com>
2019-04-06 14:11:42 +0200

    template literals

---
+++ index.js
@@ -0,0 +1,14 @@
+let word1 = 'Dylan';
+let word2 = 'Israel';
+let num1 = 2;
+let num2 = 3;
+
+const fullName = `${num1 + num2} ${word2}`;
+console.log(fullName);
+
+// 2 lines
+let example = `${word1} 
+${word2}`;
+
+console.log(example)
+document.getElementById('example').innerText = example;
